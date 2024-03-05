0889cd7f73c4cfba2643f4d313b668b7de1362ed
F77 <3532441585@qq.com>
2024-03-05 22:35:37 +0800

    后端v1.0.3 添加服务器端部分文件

--- server-go/controller/proxy.go
+++ server-go/controller/proxy.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"net"
 )
@@ -16,29 +17,48 @@ func InitProxy() {
 		fmt.Println("Listen Error! ", err)
 	}
 	defer listener.Close()
-	// 接受客户端连接
+	// 不断接受客户端连接
+	go func() {
+		for {
+			ConnAccept(listener)
+		}
+	}()
+}
+
+// 接受客户端连接
+func ConnAccept(listener net.Listener) {
 	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Failed to create connection. ", err)
 	}
 	defer conn.Close()
-	// 读取发送消息
-	/*
-		go func() {
-			// 创建读写缓冲区
-			reader := bufio.NewReader(conn)
-			writer := bufio.NewWriter(conn)
-			// readers["new user"]=reader
-			// writers["1"]=writer
-			// 读取消息
-			go func() {
+	// 获取客户端用户名
+	reader := bufio.NewReader(conn)
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Socket recive error ", err)
+	}
+	var userinfo map[string]string
+	json.Unmarshal(buffer[:n], &userinfo)
+	user := userinfo["user"]
+	// 准备接收消息
+	writer := bufio.NewWriter(conn)
+	readers[user] = reader
+	writers[user] = writer
+	// 另起一个线程接收消息
+	go func() {
+		for {
+			message, _ := reader.ReadString('\n')
+			fmt.Println("Get %s message: %s", user, message)
+			// 处理接收到的消息
+		}
+	}()
+}
 
-			}()
-			// 发送消息
-			for {
-				writer.WriteString("new message")
-				writer.Flush()
-			}
-		}()
-	*/
+// 发送消息
+func ConnSend(user string, msg string) {
+	writer := writers[user]
+	writer.Write([]byte(msg))
+	writer.Flush()
 }
